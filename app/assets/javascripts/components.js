4f155da8b2416321a9cd391e48dc4e7bfbb797d5
zheng <zheng@sansan.com>
2017-01-08 21:12:18 +0900

    :wrench: 必要なpackage修正

---
+++ app/assets/javascripts/components.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomeRoot from './components/components/home_root';
+
+global.React = React
+global.ReactDOM = ReactDOM
+global.HomeRoot = HomeRoot
+
