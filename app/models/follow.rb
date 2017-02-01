9c1a8b1852de9286950dfb84b10bed33dd390e7d
zheng <zheng@sansan.com>
2017-02-02 02:09:16 +0900

    :wrench: add validates

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,6 +2,9 @@ class Follow < ApplicationRecord
   after_create :increment_profile_follow_counts
   after_destroy :decrement_profile_follow_counts
 
+  validates :following_user_id, presence: true, uniqueness: {scope: :followed_user_id}
+  validates :followed_user_id, presence: true
+
   def increment_profile_follow_counts
     Profile.find_by(user_id: self.following_user_id).try(:increment!, :following_count)
     Profile.find_by(user_id: self.followed_user_id).try(:increment!, :followed_count)
